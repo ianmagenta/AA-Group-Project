13dd80f0e528b6108544d5c4804c796ba50b5ff9
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-04-21 13:14:24 -0700

    for pull

---
+++ public/js/newstory.js
@@ -0,0 +1,7 @@
+let editorTitle = new MarkdownEditor('#markdown-title-editor');
+let editorSubheader = new MarkdownEditor('#markdown-subheader-editor');
+let editorStory = new MarkdownEditor('#markdown-story-editor');
+
+editorTitle.render('auto');
+editorSubheader.render('auto');
+editorStory.render('auto');

af67300e72839e24a679671bf57c601339967d1b
Ian Magenta <ianmagenta@gmail.com>
2020-04-22 11:00:27 -0500

    Fixed session issues

---
+++ public/js/signin.js
@@ -0,0 +1,33 @@
+import { handleErrors } from "./utils.js";
+const registerForm = document.querySelector(".register-form");
+
+registerForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(registerForm);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    const body = { email, password };
+
+    try {
+        const res = await fetch("http://localhost:8080/session", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+
+        if (!res.ok) {
+            throw res;
+        }
+
+        // JWT???
+        // const { token, user: { id }, } = await res.json();
+        // localStorage.setItem("RARE_ACCESS_TOKEN", token);
+        // localStorage.setItem("RARE_USER_ID", id);
+        window.location.href = "/";
+    } catch (err) {
+        handleErrors(err);
+    }
+})

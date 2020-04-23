b03d32afd4de0c96de3afbcc8b6d0f43849e9794
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-04-23 13:28:42 -0700

    remove footer, start search page work

---
+++ public/js/search.js
@@ -0,0 +1,9 @@
+import { handleErrors } from "./utils.js";
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  const articlesSearchContainer = document.querySelector(".articles-search-container");
+  const usersSearchContainer = document.querySelector(".users-search-container");
+  // "".site - button"
+
+
+});

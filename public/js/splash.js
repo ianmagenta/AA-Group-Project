77073b3ed16e8199889a043a367cfe330a82c4c1
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-04-24 15:05:19 -0700

    Splash loads categories

--- public/js/splash.js
+++ public/js/splash.js
@@ -1,33 +1,25 @@
+import { handleErrors, generateCategoriesHTML } from "./utils.js";
+
 const signInOrUsername = document.querySelector(".sign-in-top");
 const RegisterOrLogout = document.querySelector(".register-top");
 signInOrUsername.innerHTML = `<a href="/login">Sign in</a>`;
 RegisterOrLogout.innerHTML = `<a href="/register">Register</a>`;
+const categoryContainer = document.querySelector(".mid-container");
 
-// document.addEventListener("DOMContentLoaded", async () => {
-//   const userId = localStorage.getItem("RARE_USER_ID");
-//   // console.log('this runs');
-//   try {
-//     // if (userId) {
-//     const res = await fetch(`http://localhost:8080/users/${userId}`, { headers: { Authorization: `Bearer ${localStorage.getItem("RARE_ACCESS_TOKEN")}` } });
-//     const data = await res.json();
-//     // console.log(data);
-//     const { user } = data;
-//     // console.log(user);
-//     if (res.status >= 401) {
-//       window.location.href = "./login";
-//       return;
-//     }
+document.addEventListener("DOMContentLoaded", async () => {
+  const userId = localStorage.getItem("RARE_USER_ID");
+  // console.log('this runs');
+  try {
 
-//     if (res.ok) {
-//       RegisterOrLogout.addEventListener("click", (e) => {
-//         localStorage.removeItem("RARE_ACCESS_TOKEN");
-//         localStorage.removeItem("RARE_USER_ID");
-//       });
-//       signInOrUsername.innerHTML = `<a href="/profile/${userId}">${user.userName}</a>`;
-//       RegisterOrLogout.innerHTML = `<a href="/splash">Logout</a>`;
-//     }
-//     // }
-//   } catch (err) {
-//     handleErrors(err);
-//   }
-// });
+    const res = await fetch(`http://localhost:8080/storycategories/`);
+    const data = await res.json();
+    const { categories } = data;
+
+    if (res.ok) {
+      categoryContainer.innerHTML = generateCategoriesHTML(categories);
+    }
+
+  } catch (err) {
+    handleErrors(err);
+  }
+});

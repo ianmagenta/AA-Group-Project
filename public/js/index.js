5eb55e07a97d7a0e5b246af5b45898ab232df855
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-04-24 15:20:35 -0700

    Add categories loading to index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,10 @@
-import { handleErrors, generateArticleHtml, generateUserHtml } from "./utils.js";
+import { handleErrors, generateArticleHtml, generateUserHtml, generateCategoriesHTML } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", async (e) => {
   const topStoriesContainer = document.querySelector(".top-stories-container");
   const mainStoriesLeft = document.querySelector(".main-stories-left");
   const mainStoriesRight = document.querySelector(".main-stories-right");
+  const categoryContainer = document.querySelector(".categories-container");
 
   try {
     const res = await fetch(`http://localhost:8080/story`, { headers: { Authorization: `Bearer ${localStorage.getItem("RARE_ACCESS_TOKEN")}` } });
@@ -21,6 +22,19 @@ document.addEventListener("DOMContentLoaded", async (e) => {
     //capture the rest of the stories and put in left container
     let restOfStories = stories.slice(6);
     mainStoriesLeft.innerHTML = `${generateArticleHtml(restOfStories)}`;
+  } catch (e) {
+    handleErrors(e);
+  }
+
+  //get categories and place in top container
+  try {
+    const res = await fetch(`http://localhost:8080/storycategories/`);
+    const data = await res.json();
+    const { categories } = data;
+    let categories2 = categories.slice(0, 6);
+    if (res.ok) {
+      categoryContainer.innerHTML = generateCategoriesHTML(categories2);
+    }
   } catch (e) {
     handleErrors(e);
   }

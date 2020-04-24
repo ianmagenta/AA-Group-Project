844361ac2bab067816e79aa0be0559097e639ca4
Ian Magenta <ianmagenta@gmail.com>
2020-04-24 18:16:56 -0500

    finalized heroku changes

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
+const environment = process.env.NODE_ENV || "development"
+
 module.exports = {
-  environment: process.env.NODE_ENV || "development",
-  api: process.env.NODE_ENV === "development" ? process.env.DEV_API : process.env.PROD_API,
+  environment,
+  api: environment === "development" ? process.env.DEV_API : process.env.PROD_API,
   port: process.env.PORT || 8080,
   db: {
     username: process.env.DB_USERNAME,

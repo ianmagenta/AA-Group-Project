d2b82b76c81b78f6d03699630afafb6770f46e05
Ian Magenta <ianmagenta@gmail.com>
2020-04-24 19:03:16 -0500

    final commit for the day

--- public/js/signin.js
+++ public/js/signin.js
@@ -1,11 +1,40 @@
 import { handleErrors, api } from "./utils.js";
 const registerForm = document.querySelector(".register-form");
+const guestForm = document.querySelector(".guest-login-form");
 
 registerForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const formData = new FormData(registerForm);
     const userName = formData.get("userName");
     const password = formData.get("password");
+
+    const body = { userName, password };
+
+    try {
+        console.log(`${api}session`);
+        const res = await fetch(`${api}session`, {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        if (!res.ok) {
+            throw res;
+        }
+        const { token, user: { id }, } = await res.json();
+        localStorage.setItem("RARE_ACCESS_TOKEN", token);
+        localStorage.setItem("RARE_USER_ID", id);
+        window.location.href = "/";
+    } catch (err) {
+        handleErrors(err);
+    }
+});
+
+guestForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const userName = "GuestUser";
+    const password = "P@ssw0rd";
 
     const body = { userName, password };
 

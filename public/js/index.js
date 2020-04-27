c6895ee6f217756614d74bcbdee82ce5e2837316
Ian Magenta <ianmagenta@gmail.com>
2020-04-27 17:04:53 -0500

    changed the way popular stories are generated

--- public/js/index.js
+++ public/js/index.js
@@ -37,7 +37,8 @@ document.addEventListener("DOMContentLoaded", async (e) => {
       return b.storyLikes.length - a.storyLikes.length;
     });
 
-    let nextThreeStories = stories.splice(0, 6);
+    const halfStories = Math.round(stories.length / 2);
+    let nextThreeStories = stories.splice(0, halfStories);
     mainStoriesRight.innerHTML = `<div class="heading-text text-style1">Popular Stories</div>
     ${generateArticleHtml(nextThreeStories)}`;
 
